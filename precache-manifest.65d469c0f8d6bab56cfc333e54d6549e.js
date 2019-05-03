b03a748c6664917f3167f4e3018d2b41edda3472
JordanFrontDev <38885757+JordanFrontDev@users.noreply.github.com>
2019-05-03 18:41:55 -0300

    Updates

---
+++ precache-manifest.65d469c0f8d6bab56cfc333e54d6549e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e664ce36d7f9e4d5bfe0c433ebe5170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edff9507dccecd79a7f3",
+    "url": "/static/css/main.7c5b8cbe.chunk.css"
+  },
+  {
+    "revision": "0fc7b728dc7b89798648",
+    "url": "/static/js/2.bc125a14.chunk.js"
+  },
+  {
+    "revision": "edff9507dccecd79a7f3",
+    "url": "/static/js/main.054e8b19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
