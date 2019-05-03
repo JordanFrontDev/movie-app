a98be705cf58f30c451856db585cb82477092b6f
JordanFrontDev <38885757+JordanFrontDev@users.noreply.github.com>
2019-05-03 18:06:04 -0300

    Updates

---
+++ precache-manifest.d9cc7c5fc30fe67cec7f0630800321b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b52533d17a38a12e44f6a551fb499ae",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "c39a654ecf57617f6842",
+    "url": "/movie-app/static/css/main.7c5b8cbe.chunk.css"
+  },
+  {
+    "revision": "0fc7b728dc7b89798648",
+    "url": "/movie-app/static/js/2.bc125a14.chunk.js"
+  },
+  {
+    "revision": "c39a654ecf57617f6842",
+    "url": "/movie-app/static/js/main.5e4eb7e0.chunk.js"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  }
+]);
